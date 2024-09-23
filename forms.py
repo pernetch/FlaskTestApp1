400bd2252fa39caf8fc09e57d4b061204a70ebad
Christian Pernet <christian.pernet@hotmail.com>
2024-09-23 20:11:23 +0200

    modified:   forms.py

--- forms.py
+++ forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+#simple formulaire
 class NameForm(FlaskForm):
     name = StringField('Indiquez votre nom',validators=[DataRequired()])
     submit = SubmitField('Submit')
